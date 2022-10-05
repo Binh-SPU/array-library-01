dfb13298424ea7092c770724885d3888dd8345ed
Carlos Roberto Arias Arevalo <arias@spu.edu>
2022-10-05 14:21:02 -0700

    some more functions

--- array_library.cpp
+++ array_library.cpp
@@ -13,4 +13,69 @@ int LinearSearch(int array[], unsigned int size, int key){
     return -1;
 }
 
+int FindMax(int array[], unsigned int size){
+    int max = array[0];
+    for (int i = 1; i < size; ++i) {
+        if (max < array[i])
+            max = array[i];
+    }
+    return max;
+}
+int FindMin(int array[], unsigned int size){
+    int min = array[0];
+    for (int i = 1; i < size; ++i) {
+        if (min > array[i])
+            min = array[i];
+    }
+    return min;
+}
+double FindAverage(int array[], unsigned int size){
+    double sum = 0.0;
+    for (int i = 0; i < size; ++i) {
+        sum += array[i];
+    }
+    return sum / size;
+}
+unsigned int MinimumPosition(int array[], unsigned int size, unsigned int startPosition = 0){
+//    unsigned minPosition = 0;
+//    for (int i = startPosition; i < size; ++i) {
+//        if (array[minPosition] > array[i])
+//            minPosition = i;
+//    }
+//    return minPosition;
+    int minimum = array[0];
+    unsigned int minimumPosition = startPosition;
+    for (int i = startPosition; i < size; ++i) {
+        if (minimum > array[i]){
+            minimum = array[i];
+            minimumPosition = i;
+        }
+    }
+    return minimumPosition;
+}
+void SelectionSort(int array[], unsigned int size){
+
+}
+double FindMedian(int array[], unsigned int size){
+    SelectionSort(array, size);
+    if (size % 2 == 0)
+        return (static_cast<double>(array[size/2]) + static_cast<double>(array[size/2]+ 1)) / 2.0;
+    else
+        return array[size / 2];
+}
+int BinarySearch(int array[], unsigned int size, int key){
+    // Someday...
+}
+void PrintArray(ostream& output, int array[], unsigned int size){
+    for (int i = 0; i < size; ++i) {
+        output << array[i] << " ";
+    }
+}
+void ReadArray(istream& input, int array[], unsigned int size){
+    for (int i = 0; i < size; ++i) {
+        input >> array[i];
+    }
+}
+
+
 
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 
 #include "array_library.h"
 
@@ -8,10 +9,29 @@ using std::endl;
 
 int main() {
     int data[] = {7, 4, 1, 9, 0, 11, 21, 3, 8, 5};
+    // Average 6.9
+    // Sorted: 0 1 3 4 5 7 8 9 11 21
+    // Median 6
+
     const unsigned int SIZE = sizeof(data) / sizeof (data[0]);
 
-    int position = LinearSearch(data, SIZE, 8);
-    cout << "8 found in position " << position << endl;
+    assert(LinearSearch(data, SIZE, 7) == 0);
+    assert(LinearSearch(data, SIZE, 4) == 1);
+    assert(LinearSearch(data, SIZE, 1) == 2);
+    assert(LinearSearch(data, SIZE, 9) == 3);
+    assert(LinearSearch(data, SIZE, 0) == 4);
+    assert(LinearSearch(data, SIZE, 11) == 5);
+    assert(LinearSearch(data, SIZE, 21) == 6);
+    assert(LinearSearch(data, SIZE, 3) == 7);
+    assert(LinearSearch(data, SIZE, 8) == 8);
+    assert(LinearSearch(data, SIZE, 5) == 9);
+
+    assert(LinearSearch(data, SIZE, 10) == -1);
+
+    assert(FindMin(data, SIZE) == 0);
+    assert(FindMax(data, SIZE) == 21);
+
+    assert(FindAverage(data, SIZE) - 6.9 < 0.000001);
 
     return 0;
 }
